7d23f1cd069c13b8c824ba575f7c16536d0ddd34
ChenChao <chea97@qq.com>
2020-03-16 19:28:17 +0800

    resset样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Index from 'views/Index.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+/* 路由懒加载 */
+const Index = () => import("views/Index.vue");
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'index',
+    path: "/",
+    name: "index",
     component: Index
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
